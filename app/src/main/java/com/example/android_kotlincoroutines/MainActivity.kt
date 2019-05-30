198b9089d4088b300e50eeecc911d8b14c5a9a2e
KyleCole01 <kyle@misfitselectronics.com>
2019-05-30 15:55:05 -0400

    added coroutines to a boredapi based app I was working on to figure out list adapters

---
+++ app/src/main/java/com/example/android_kotlincoroutines/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.android_kotlincoroutines
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val viewAdapter = DiversionListAdapter()
+        val viewManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+        findViewById<RecyclerView>(R.id.recycler_view).apply {
+            setHasFixedSize(false)
+
+            layoutManager = viewManager
+
+            adapter = viewAdapter
+        }
+    }
+}
